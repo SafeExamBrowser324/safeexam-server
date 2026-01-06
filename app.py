747edaa93cb4d86a111879b02fd261960e9f08c8
SafeExamBrowser324 <clemens.d-n@gmx.de>
2026-01-06 22:48:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -45,3 +46,4 @@ if __name__ == "__main__":
     # Für lokal UND Render geeignet
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
